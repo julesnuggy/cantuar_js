ccb3c0a81bfe8414f7f016bef49c70fb83a79d32
julesnuggy <julesnuggy@gmail.com>
2018-06-06 15:49:53 +0100

    Next Random Card button fixed - session history clears on visiting Index page

--- index.js
+++ index.js
@@ -21,7 +21,12 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.render('index.ejs'));
+app.get('/', (req, res) => {
+  if (session !== undefined) {
+    session.clearHistory();
+  }
+  res.render('index.ejs')
+});
 
 app.get('/create', (req, res) => res.render('create.ejs'));
 
@@ -29,25 +34,24 @@ app.post('/deck', (req, res) => {
   flashcard.create(req.body.original, req.body.translation);
   session = new Session(flashcard._object);
   res.redirect('/deck');
-})
+});
 
 app.get('/deck', (req, res) => {
   res.render('deck.ejs', { data: flashcard._object } );
-})
+});
 
 app.get('/practice', (req, res) => {
-  session.clearHistory();
   selectedCard = session.select();
   res.render('practice.ejs', { data: selectedCard });
-})
+});
 
 app.post('/practice', (req, res) => {
   showHide('practice_translation');
-})
+});
 
 app.get('/clear_home', (req,res) => {
   session.clearHistory();
   res.redirect('/')
-})
+});
 
 app.listen(process.env.PORT || 3000, () => console.log(`App is now running from port ${process.env.PORT || 3000}`));
